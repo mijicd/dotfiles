18c29576c26b45e9de019299f2614b547a5c6985
mijicd <dejan.mjc@gmail.com>
2014-05-15 18:00:37 +0200

    -- Vi keymap added.

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -1,15 +1,8 @@
-# To see the key combo you want to use just do:
-# cat > /dev/null
-# And press it
+# use vi settings
+bindkey -v
 
-bindkey "^K"      kill-whole-line                      # ctrl-k
-bindkey "^R"      history-incremental-search-backward  # ctrl-r
-bindkey "^A"      beginning-of-line                    # ctrl-a  
-bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "[B"      history-search-forward               # down arrow
-bindkey "[A"      history-search-backward              # up arrow
-bindkey "^D"      delete-char                          # ctrl-d
-bindkey "^F"      forward-char                         # ctrl-f
-bindkey "^B"      backward-char                        # ctrl-b
-bindkey -v   # Default to standard vi bindings, regardless of editor string
+# remap escape key
+bindkey -M viins 'jj' vi-cmd-mode
 
+# history navigation
+bindkey '^R' history-incremental-search-backward
