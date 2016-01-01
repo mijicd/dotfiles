35203ead34684ca4cad5ea26381104540b023828
Dejan Mijic <dejan.mjc@gmail.com>
2016-01-01 22:16:37 +0100

    Added anaconda reference.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,5 @@
 # initialize path
-PATH=/usr/local/bin:/usr/local/sbin:$HOME/.rvm/bin:$PATH
-
-# grep colors
-# export GREP_OPTIONS='--color=auto'
-# export GREP_COLOR='3;33'
+PATH=/usr/local/bin:/usr/local/sbin:$HOME/.anaconda3/bin:$PATH
 
 # set editor and pager
 export LESS='--ignore-case --raw-control-chars'
@@ -12,15 +8,9 @@ export VISUAL=vim
 export EDITOR=vim
 
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
-export LC_COLLATE=C 
+export LC_COLLATE=C
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
-# python virtual envirinoment setup
-export WORKON_HOME=$HOME/.virtualenvs
-[[ -s "/usr/bin/virtualenvwrapper.sh" ]] && source /usr/bin/virtualenvwrapper.sh 
-# start rvm
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # libraries
 if [[ $IS_LINUX -eq 1 ]]; then
