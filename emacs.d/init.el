4cd378f63a7048b149a497f558ac178abd428514
Dejan Mijic <dejan.mijic@exproit.rs>
2016-03-08 11:40:37 +0100

    Customized emacs powerline.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -349,7 +349,18 @@
   (setq ns-use-srgb-colorspace nil))
 
 ;; UI
-(use-package gruvbox-theme :ensure t :defer t)
-(load-theme 'gruvbox)
+(use-package zenburn-theme :ensure t :defer t)
+(use-package airline-themes :ensure t :defer t)
 
+(load-theme 'zenburn)
+(load-theme 'airline-badwolf)
+(set-frame-font "Monaco-14")
+
+(setq airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
 (provide 'init)
