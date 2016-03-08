63b96d2ef93dad7cf55dc69bcb91efa30bcf530f
Dejan Mijic <dejan.mijic@exproit.rs>
2016-03-08 11:18:23 +0100

    Added emacs configuration based on Aaron Bieber's.

---
+++ emacs.d/init.el
@@ -0,0 +1,355 @@
+;;; "root" configuration file
+
+(add-to-list 'load-path (expand-file-name "src" user-emacs-directory))
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+; (add-to-list 'exec-path "/usr/local/bin")
+
+(require 'package)
+(package-initialize)
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; essential settings
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(when (boundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(show-paren-mode 1)
+(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
+(setq-default left-fringe-width nil)
+(setq-default indent-tabs-mode nil)
+(eval-after-load "vc" '(setq vc-handled-backends nil))
+(setq vc-follow-symlinks t)
+(setq large-file-warning-threshold nil)
+(setq split-width-threshold nil)
+(setq visible-bell nil)
+(setq custom-safe-themes t)
+(put 'narrow-to-region 'disabled nil)
+
+(defun my-minibuffer-setup-hook ()
+  "Increase GC cons threshold."
+  (setq gc-cons-threshold most-positive-fixnum))
+
+(defun my-minibuffer-exit-hook ()
+  "Set GC cons threshold to its default value."
+  (setq gc-cons-threshold 1000000))
+
+(add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
+(add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
+
+(defvar backup-dir "~/.emacs.d/backups/")
+(setq backup-directory-alist (list (cons "." backup-dir)))
+(setq make-backup-files nil)
+
+;;; remap file types
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.twig$" . web-mode))
+
+;;; load personal configuration
+(require 'init-utils)
+(require 'init-global-functions)
+(require 'init-elpa)
+(require 'diminish)
+(require 'bind-key)
+(require 'init-gtags)
+(require 'init-evil)
+(require 'init-org)
+(require 'init-maps)
+(require 'init-w3m)
+(require 'init-powerline)
+(require 'init-flycheck)
+
+;; Org prerequisites
+(use-package visual-fill-column
+  :ensure t)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package elpy
+  :ensure t)
+
+(use-package rainbow-mode
+  :ensure t)
+
+(use-package css-mode
+  :ensure t
+  :config
+  (add-hook 'css-mode-hook (lambda ()
+                             (rainbow-mode))))
+
+(use-package wgrep
+  :ensure t
+  :config
+  (setq wgrep-auto-save-buffer t))
+
+(use-package wgrep-ag
+  :ensure t
+  :commands (wgrep-ag-setup))
+
+(use-package ag
+  :ensure t
+  :defer t
+  :config
+  (add-hook 'ag-mode-hook
+            (lambda ()
+              (wgrep-ag-setup)
+              (define-key ag-mode-map (kbd "n") 'evil-search-next)
+              (define-key ag-mode-map (kbd "N") 'evil-search-previous)))
+  (setq ag-executable "/usr/local/bin/ag")
+  (setq ag-highlight-search t)
+  (setq ag-reuse-buffers t)
+  (setq ag-reuse-window t))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :defer t
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
+
+(use-package helm
+  :ensure t
+  :diminish helm-mode
+  :config
+  (helm-mode 1)
+  (setq helm-buffers-fuzzy-matching t)
+  (setq helm-autoresize-mode t)
+  (setq helm-buffer-max-length 40)
+  (define-key helm-map (kbd "S-SPC") 'helm-toggle-visible-mark)
+  (define-key helm-find-files-map (kbd "C-k") 'helm-find-files-up-one-level))
+
+(use-package company
+  :ensure t
+  :defer t
+  :init
+  (global-company-mode)
+  :config
+  (setq company-idle-delay 0.2)
+  (setq company-selection-wrap-around t)
+  (define-key company-active-map [tab] 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+(use-package swiper
+  :ensure t
+  :commands swiper
+  :bind ("C-s" . swiper))
+
+(use-package dictionary :ensure t)
+(use-package emmet-mode :ensure t)
+(use-package flycheck :ensure t)
+(use-package helm-projectile
+  :commands (helm-projectile helm-projectile-switch-project)
+  :ensure t)
+
+(use-package markdown-mode
+  :bind (("C-\\" . markdown-insert-list-item)
+         ("C-c '" . fence-edit-code-at-point)
+         ("C-c 1" . markdown-insert-header-atx-1)
+         ("C-c 2" . markdown-insert-header-atx-2)
+         ("C-c 3" . markdown-insert-header-atx-3)
+         ("C-c 4" . markdown-insert-header-atx-4)
+         ("C-c 5" . markdown-insert-header-atx-5)
+         ("C-c 6" . markdown-insert-header-atx-6))
+  :ensure t)
+
+(use-package web-mode
+  :ensure t
+  :defer t
+  :config
+  (setq web-mode-attr-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-indent-style 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2))
+
+(use-package mmm-mode :ensure t :defer t)
+(use-package yaml-mode :ensure t :defer t)
+
+(use-package yasnippet
+  :ensure t
+  :defer t
+  :config
+  (yas-reload-all)
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"
+                           "~/.emacs.d/remote-snippets"))
+  (setq tab-always-indent 'complete)
+  (setq yas-prompt-functions '(yas-completing-prompt
+                               yas-ido-prompt
+                               yas-dropdown-prompt))
+  (define-key yas-minor-mode-map (kbd "<escape>") 'yas-exit-snippet))
+
+(use-package which-key
+  :ensure t
+  :diminish ""
+  :config
+  (which-key-mode t))
+
+(use-package projectile
+  :ensure t
+  :defer t
+  :config
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
+
+(use-package highlight-symbol
+  :ensure t
+  :defer t
+  :diminish ""
+  :config
+  (setq-default highlight-symbol-idle-delay 1.5))
+
+(use-package magit
+  :ensure t
+  :defer t
+  :config
+  (setq magit-branch-arguments nil)
+  (setq magit-push-always-verify nil)
+  (setq magit-last-seen-setup-instructions "1.4.0"))
+
+(use-package mmm-mode
+  :ensure t
+  :defer t
+  :config
+  (setq mmm-global-mode 'maybe)
+  (mmm-add-classes
+   '((markdown-cl
+      :submode emacs-lisp-mode
+      :face mmm-declaration-submode-face
+      :front "^~~~cl[\n\r]+"
+      :back "^~~~$")
+     (markdown-php
+      :submode php-mode
+      :face mmm-declaration-submode-face
+      :front "^```php[\n\r]+"
+      :back "^```$")))
+  (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-cl)
+  (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-php))
+
+;; GNU PG
+(require 'epa-file)
+(epa-file-enable)
+(setq-default epa-file-cache-passphrase-for-symmetric-encryption t)
+
+(defvar show-paren-delay 0
+  "Delay (in seconds) before matching paren is highlighted.")
+
+;;; various hooks
+(add-hook 'flycheck-mode-hook
+          (lambda ()
+            (evil-define-key 'normal flycheck-mode-map (kbd "]e") 'flycheck-next-error)
+            (evil-define-key 'normal flycheck-mode-map (kbd "[e") 'flycheck-previous-error)))
+
+(add-hook 'lisp-interaction-mode-hook
+          (lambda ()
+            (define-key lisp-interaction-mode-map (kbd "<C-return>") 'eval-last-sexp)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (define-key magit-mode-map (kbd ",o") 'delete-other-windows)))
+
+(add-hook 'git-commit-mode-hook 'evil-insert-state)
+
+(add-hook 'emmet-mode-hook
+          (lambda ()
+            (evil-define-key 'insert emmet-mode-keymap (kbd "C-S-l") 'emmet-next-edit-point)
+            (evil-define-key 'insert emmet-mode-keymap (kbd "C-S-h") 'emmet-prev-edit-point)))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-style-padding 2)
+            (yas-minor-mode t)
+            (emmet-mode)
+            (flycheck-add-mode 'html-tidy 'web-mode)
+            (flycheck-mode)))
+
+(setq web-mode-ac-sources-alist
+      '(("php" . (ac-source-php-extras ac-source-yasnippet ac-source-gtags ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+        ("css" . (ac-source-css-property ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))))
+
+(add-hook 'web-mode-before-auto-complete-hooks
+          '(lambda ()
+             (let ((web-mode-cur-language (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "php")
+                   (yas-activate-extra-mode 'php-mode)
+                 (yas-deactivate-extra-mode 'php-mode))
+               (if (string= web-mode-cur-language "css")
+                   (setq emmet-use-css-transform t)
+                 (setq emmet-use-css-transform nil)))))
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (yas-minor-mode t)
+            (eldoc-mode)
+            (highlight-symbol-mode)
+            (define-key emacs-lisp-mode-map (kbd "<C-return>") 'eval-last-sexp)))
+
+(add-hook 'sh-mode-hook (lambda ()
+                          (setq sh-basic-offset 2)
+                          (setq sh-indentation 2)))
+
+(add-hook 'javascript-mode-hook (lambda ()
+                                  (set-fill-column 120)
+                                  (turn-on-auto-fill)
+                                  (setq js-indent-level 2)))
+
+(add-hook 'markdown-mode-hook (lambda ()
+                                (set-fill-column 80)
+                                (turn-on-auto-fill)
+                                (flyspell-mode)))
+
+(add-hook 'html-mode-hook (lambda ()
+                            (setq sgml-basic-offset 2)
+                            (setq indent-tabs-mode nil)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode which-key wgrep-ag web-mode w3m use-package rainbow-mode powerline-evil org-bullets mmm-mode markdown-mode magit highlight-symbol helm-projectile gtags fullframe flycheck-package exec-path-from-shell evil-surround evil-leader evil-jumper evil-indent-textobject emmet-mode elpy dictionary bpr avy auto-complete ag)))
+ '(safe-local-variable-values (quote ((no-byte-compile t)))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#282828" :foreground "#fdf4c1")))))
+
+(put 'narrow-to-region 'disabled nil)
+(diminish 'undo-tree-mode)
+(require 'init-linum)
+
+;;; sRGB doesn't blend with Powerline's pixmap colors, but is only
+;;; used in OS X. Disable sRGB before setting up Powerline.
+(when (memq window-system '(mac ns))
+  (setq ns-use-srgb-colorspace nil))
+
+;; UI
+(use-package gruvbox-theme :ensure t :defer t)
+(load-theme 'gruvbox)
+
+(provide 'init)
---
+++ emacs.d/src/init-org.el
@@ -0,0 +1,155 @@
+;;; init-org.el --- Set up Org Mode
+;;; Commentary:
+
+;; Basic Org Mode configuration, assuming presence of Evil & Evil Leader.
+
+;;; Code:
+(use-package org
+  :ensure t
+  :defer t
+  :commands (org-capture)
+  :bind (("C-c c" . air-org-task-capture)
+         ("C-c t n" . air-pop-to-org-notes)
+         ("C-c t t" . air-pop-to-org-todo)
+         ("C-c t v" . air-pop-to-org-vault)
+         ("C-c t a" . air-pop-to-org-agenda)
+         ("C-c t A" . org-agenda)
+         ("C-c f k" . org-search-view)
+         ("C-c f t" . org-tags-view))
+  :config
+  (setq org-agenda-text-search-extra-files '(agenda-archives))
+  (setq org-todo-keywords
+        '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
+
+  (setq org-blank-before-new-entry '((heading . t)
+                                     (plain-list-item . t)))
+  (setq org-enforce-todo-dependencies t)
+  (setq org-log-done (quote time))
+  (setq org-log-redeadline (quote time))
+  (setq org-log-reschedule (quote time))
+
+  (evil-leader/set-key-for-mode 'org-mode
+    "$"  'org-archive-subtree
+    "a"  'org-agenda
+    "d"  'org-deadline
+    "ns" 'org-narrow-to-subtree
+    "p"  'org-set-property
+    "s"  'org-schedule
+    "u"  'org-up-element
+    "t"  'air-org-set-tags)
+
+  (defun air-org-insert-scheduled-heading ()
+    "Insert a new org heading scheduled for today."
+    (interactive)
+    (call-interactively 'org-insert-todo-heading)
+    (org-schedule nil (format-time-string "%Y-%m-%d")))
+
+  (defun air-org-task-capture ()
+    "Capture a task with my default template."
+    (interactive)
+    (org-capture nil "a"))
+
+  (defun air-org-agenda-capture ()
+    "Capture a task in agenda mode, using the date at point."
+    (interactive)
+    (let ((org-overriding-default-time (org-get-cursor-date)))
+      (org-capture nil "a")))
+
+  (defun air-org-insert-list-leader-or-self (char)
+    (if (= (current-column) 0)
+        (insert (concat " " char " "))
+      (insert char)))
+
+  (defun air--org-swap-tags (tags)
+    "Replace any tags on the current headline with TAGS.
+The assumption is that TAGS will be a string conforming to Org Mode's
+tag format specifications, or nil to remove all tags."
+    (let ((old-tags (org-get-tags-string))
+          (tags (if tags
+                    (concat " " tags)
+                  "")))
+      (save-excursion
+        (beginning-of-line)
+        (re-search-forward
+         (concat "[ \t]*" (regexp-quote old-tags) "[ \t]*$")
+         (line-end-position) t)
+        (replace-match tags)
+        (org-set-tags t))))
+
+  (defun air-org-set-tags (tag)
+    "Add TAG if it is not in the list of tags, remove it otherwise.
+TAG is chosen interactively from the global tags completion table."
+    (interactive
+     (list (let ((org-last-tags-completion-table
+                  (if (derived-mode-p 'org-mode)
+                      (org-uniquify
+                       (delq nil (append (org-get-buffer-tags)
+                                         (org-global-tags-completion-table))))
+                    (org-global-tags-completion-table))))
+             (org-icompleting-read
+              "Tag: " 'org-tags-completion-function nil nil nil
+              'org-tags-history))))
+    (let* ((cur-list (org-get-tags))
+           (new-tags (mapconcat 'identity
+                                (if (member tag cur-list)
+                                    (delete tag cur-list)
+                                  (append cur-list (list tag)))
+                                ":"))
+           (new (if (> (length new-tags) 1) (concat " :" new-tags ":")
+                  nil)))
+      (air--org-swap-tags new)))
+
+  (add-hook 'org-agenda-mode-hook
+            (lambda ()
+              (setq org-habit-graph-column 50)
+              (define-key org-agenda-mode-map "j"         'org-agenda-next-line)
+              (define-key org-agenda-mode-map "k"         'org-agenda-previous-line)
+              (define-key org-agenda-mode-map "n"         'org-agenda-next-date-line)
+              (define-key org-agenda-mode-map "p"         'org-agenda-previous-date-line)
+              (define-key org-agenda-mode-map "c"         'air-org-agenda-capture)
+              (define-key org-agenda-mode-map (kbd "RET") 'org-agenda-switch-to)))
+
+  (add-hook 'org-capture-mode-hook
+            (lambda ()
+              (evil-insert-state)))
+
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (setq-local my-timer
+                          (run-with-idle-timer 1 t
+                                               (lambda ()
+                                                 (when (and (eq major-mode 'org-mode)
+                                                            (and evil-state
+                                                                 (not (eq evil-state 'insert)))
+                                                            (buffer-file-name)
+                                                            (buffer-modified-p))
+                                                   (save-buffer)))))
+              ;; Special plain list leader inserts
+              (dolist (char '("+" "-"))
+                (define-key org-mode-map (kbd char)
+                  `(lambda ()
+                    (interactive)
+                    (air-org-insert-list-leader-or-self ,char))))
+              ;; Normal maps
+              (define-key org-mode-map (kbd "C-c ,") 'org-time-stamp-inactive)
+              (define-key org-mode-map (kbd "C-|") 'air-org-insert-scheduled-heading)
+              (define-key org-mode-map (kbd "C-<") 'org-metaleft)
+              (define-key org-mode-map (kbd "C->") 'org-metaright)
+              (define-key org-mode-map (kbd "C-\\") 'org-insert-heading)
+              (define-key org-mode-map (kbd "C-S-j") 'org-priority-down)
+              (define-key org-mode-map (kbd "C-S-k") 'org-priority-up)
+              (evil-define-key 'normal org-mode-map (kbd "TAB") 'org-cycle)
+              ;; Use fill column, but not in agenda
+              (setq fill-column 100)
+              (when (not (eq major-mode 'org-agenda-mode))
+                (visual-line-mode)
+                (visual-fill-column-mode))
+              (flyspell-mode)
+              (org-indent-mode))))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(provide 'init-org)
