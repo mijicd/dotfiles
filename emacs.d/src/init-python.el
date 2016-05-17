fe9c718f73d1744b2be1e6b72bb214cd62313437
Dejan Mijic <dejan.mijic@exproit.rs>
2016-05-18 01:23:05 +0200

    Removed unused emacs config.

--- emacs.d/src/init-python.el
+++
@@ -1,8 +0,0 @@
-(use-package elpy :ensure t)
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-
-(provide 'init-python)
-
