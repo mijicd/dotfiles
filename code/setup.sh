409d73208b3511e4b451a2431a6882b6dade7bc3
Dejan Mijic <dejan.mijic@zalando.de>
2019-04-01 15:26:17 +0200

    Fix VSCode setup

--- code/setup.sh
+++ code/setup.sh
@@ -1,4 +1,4 @@
 ln -s ~/.dotfiles/code/keybindings.json ~/.config/Code/User/keybindings.json
 ln -s ~/.dotfiles/code/settings.json ~/.config/Code/User/settings.json
 
-code --list-extensions | xargs -L 1 echo code --install-extension
+cat extensions | xargs -L 1 code --install-extension
