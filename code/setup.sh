15fd681f4d881f42b4d68dd70eaaf3f42ee2b671
Dejan Mijic <dmijic@acm.org>
2019-06-30 15:59:53 +0200

    Remove VS code from toolchain

--- code/setup.sh
+++
@@ -1,4 +0,0 @@
-ln -s ~/.dotfiles/code/keybindings.json ~/.config/Code/User/keybindings.json
-ln -s ~/.dotfiles/code/settings.json ~/.config/Code/User/settings.json
-
-cat extensions | xargs -L 1 code --install-extension
