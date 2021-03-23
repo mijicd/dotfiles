e5c832b537322f3e146c262beb4b71a2067d2fb9
Dejan Mijic <dmijic@acm.org>
2021-03-23 12:10:37 +0100

    Add Coursier

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 # exports
 set -U fish_user_paths ~/.local/bin $fish_user_paths
+set -U fish_user_paths ~/.local/share/coursier/bin $fish_user_paths
 set -x LANG 'en_US.UTF-8'
 set -x LC_ALL 'en_US.UTF-8'
 set -x LESS '--ignore-case --raw-control-chars'
